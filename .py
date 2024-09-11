7e14f29b73ca020e542053a29d58ce880dbbda47
Elena.GE <ezzargi@gmail.com>
2024-09-11 18:15:40 -0300

    #

--- .py
+++ .py
@@ -1,9 +1,18 @@
-x = 13
-mensaje = x * 12
-X=123412
-12*12
+class Libro:
+    def __init__(self, ISBN, titulo, autor, año):
+        self.ISBN = ISBN
+        self.titulo = titulo
+        self.autor = autor
+        self.año = año
 
-x1= 32 
-
-x2 = 156
-y = 284
+    def encontrar_ISBN(self):
+        return self.ISBN
+    
+    def titulo_libro(self):
+        return self.titulo
+    
+    def autor_libro(self):
+        return self.autor
+    
+    def año_libro(self):
+        return self.año
