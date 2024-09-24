5cb49f01f51a51640f171da605b30a7c97cc8a94
Elena <ezzargi@gmail.com>
2024-09-24 16:15:46 -0300

    #

---
+++ BIBLIOTECA/CLASES/autor.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import paises
+
+class Autor(paises):
+    def __init__(self, id_autor, nombre_autor, seudonimo_autor, codigo_pais, fecha_ac, biografia_autor):
+        super().__init__(codigo_pais)
+        self.id_autor = id_autor
+        self.nombre_autor = nombre_autor
+        self.seudonimo_autor = seudonimo_autor
+        self.fecha_nac = fecha_nac
+        self. 
